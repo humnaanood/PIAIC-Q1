6c246999f2edf07ec14abd476e346b67a2ad9847
humnaanood <154698105+humnaanood@users.noreply.github.com>
2024-12-24 03:18:17 +0500

    Add files via upload
    String, Numbers, Boolean

---
+++ main.ts
@@ -0,0 +1,134 @@
+//let powder :string  = "lal mirch powder";
+//powder = "haldi powder ";
+//console.log(powder)
+
+//let  fullname = "HUMNAA";
+//console.log(fullname)
+//let firstname = "HUMNA ";
+//let lastname = "ANOOd";
+// console.log(firstname + " " +)
+//console.log (`my name is ${firstname} ${lastname}`)
+//console.log(`my name is 
+//${firstname}
+//${lastname}`)
+//let message = `my name is ${firstname} ${lastname}`
+//console.log (message);
+//console.log(firstname,lastname)
+
+//SOME WORDS ARE RESERVED FOR:Name,Cons...........
+
+//const number = 7;
+//console.log(number)
+//let qty = 2;
+//console.log(qty + 10)
+//console.log (qty -1)
+
+//let str1 = "1"
+//let str1 = "2"
+//console.log str"1" str"2"
+
+//let qty = 8
+//console.log(qty= 18);
+//console.log(qty- 2);
+//console.log(qty * 12000);
+//console.log(qty * 12000);
+
+//console.log(qty /3)
+//let str1 = "1"
+//let str2 = "2"
+//console.log(str1 + str2)
+
+//let n1 = 2;
+//let n2 = 3;
+//console.log(n1 ** n2)
+
+//console.log(qty % 3)
+
+//let age = 22;
+
+//console.log(age === 22);// True
+//console.log(age !=22)// False
+//console.log (age > 18)//True
+//console.log( age < 22)// False
+
+//let n =5;
+ 
+//console.log(n == 5) //true
+//console.log(n === 5) //true
+//console.log(n != 5) // false
+//console.log(n >8) // false
+//console.log(n ) // 5
+//console.log(n >8) // false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                            
